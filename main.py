faba311a2ba18b3a35a717877d82319a9de3f5d0
ivdmahesh <ivdmahesh@gmail.com>
2024-10-26 21:03:00 +0530

    added get answer function

--- main.py
+++ main.py
@@ -10,6 +10,18 @@ class Message(BaseModel):
 @app.post("/chat", description="chat with rag application through this endpoint")
 def chat(message:Message):
     try:
-        return JSONResponse(content={"message":message.message}, status_code=200)
+        response = get_answer_and_docs(message.message)
+        response_content = {
+        "question":message.message,
+        "answer":response["answer"],
+        "documents":[doc for doc in response["context"]]
+        }
+        return JSONResponse(content=response_content, status_code=200)
     except Exception as e:
         return JSONResponse(content={"message":"Exception occured: "+str(e)}, status_code=404)
+
+def get_answer_and_docs(question:str):
+    return {
+        "answer":f"answer to the question: {question}",
+        "context":[{"contextkey":"context will come here"}]
+    }
