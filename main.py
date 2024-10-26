e5f1ccf6e6d89260d2fa343e6eba6aa3f2cfb9f9
ivdmahesh <ivdmahesh@gmail.com>
2024-10-26 18:11:35 +0530

    added packages in requirements txt and also modified the function to post

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.responses import JSONResponse
+
 app = FastAPI()
 
-@app.get("/")
-def hello():
-    return {"message":"Hello TutLinks.com"}
+class Message(BaseModel):
+    message: str
+
+@app.post("/chat", description="chat with rag application through this endpoint")
+def chat(message:Message):
+    try:
+        return JSONResponse(content={"message":message.message}, status_code=200)
+    except Exception as e:
+        return JSONResponse(content={"message":"Exception occured: "+str(e)}, status_code=404)
